57b3e8c5d67acdae8317f9e202ec1ca48aac2ddd
David Hunter <david@davidhunter.io>
2024-04-21 22:08:55 -0700

    latest working setup with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import socketIOClient from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
+const ENDPOINT = 'http://127.0.0.1:8000';
 
-function App() {
+interface Document {
+  title: string;
+  link: string;
+  // Add other document properties as needed
+}
+
+const App: React.FC = () => {
+  const [documents, setDocuments] = useState<Document[]>([]);
+
+  useEffect(() => {
+    // Connect to the server using Socket.io
+    // const socket = io();
+    const socket = socketIOClient(ENDPOINT);
+
+    // Listen for the initialDocuments event
+    socket.on('initialDocuments', (initialDocuments: Document[]) => {
+      setDocuments(initialDocuments);
+    });
+
+    // Listen for the updateDocuments event
+    socket.on('updateDocuments', (updatedDocuments: Document[]) => {
+      setDocuments(updatedDocuments);
+    });
+
+    return () => {
+      // Disconnect from the server when the component unmounts
+      socket.disconnect();
+    };
+  }, []);
+  console.log('documents: ', documents);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Drudge Reader</h1>
+      <div>
+        <h2>News Stories</h2>
+        <ul>
+          {documents.map((document, index) => (
+            <li key={index}>
+              <a
+                href={document.link}
+                dangerouslySetInnerHTML={{ __html: document.link }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
