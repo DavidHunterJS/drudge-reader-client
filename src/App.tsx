4273b330f02ffe0b821ba9ef5245fcb0e71113a6
David Hunter <david@davidhunter.io>
2024-05-06 22:50:20 -0700

    testing api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 import './App.css';
-const ENDPOINT = 'http://localhost:8000';
+const ENDPOINT = 'https://trippy.wtf';
 
 interface Document {
   title: string;
