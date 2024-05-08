c387633f1e5b952925b8f575d5d61fe2afde9b1b
David Hunter <david@davidhunter.io>
2024-05-07 19:14:48 -0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const ENDPOINT = 'https://trippy.wtf';
 interface Document {
   title: string;
   link: string;
-  // Add other document properties as
+  // Add other document properties as needed
 }
 
 const App: React.FC = () => {
